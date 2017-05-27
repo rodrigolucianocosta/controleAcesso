ecc21a81a83204f32612005ca9bd40f12b343f04
rodrigo luciano costa <rodrigolucianocosta@hotmail.com>
2017-05-27 18:27:47 -0300

    replicacao dos campos de atributo iguais aos do model para criacao dos formularios

--- acessoPoint/acessoPointApp/forms.py
+++ acessoPoint/acessoPointApp/forms.py
@@ -1,7 +1,17 @@
 # -*- coding:utf-8 -*-
 from django import forms
+from models import Pessoa
 from django.contrib.localflavor.br.br_states import STATE_CHOICES	
 
 class FormPessoa(forms.Form):
 	nome = forms.CharField()
 	sobrenome = forms.CharField()
+	sexo = forms.CharField(choices=sexoOpcoes)
+	cpf = forms.CharField()
+	rg = forms.CharField()
+	orgao_expedidor = forms.CharField()
+	telefone = forms.CharField()
+
+	class Meta:
+		model = Pessoa
+		fields = ['nome','sobrenome']
